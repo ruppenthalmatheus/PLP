9b141a8a78d888c2fe78615da52d8c9c492abd27
Matheus Ruppenthal <ruppenthalmatheus@gmail.com>
2016-09-14 17:35:51 -0300

    commit exercicio IMC

---
+++ calculo_imc/calculo_imc/Pessoa.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculo_imc
+{
+    class Pessoa
+    {
+        #region Atributos
+
+        public string nome { get; set; }
+        public string sexo { get; set; }
+        public double peso { get; set; }
+        public double altura { get; set; }
+
+        #endregion
+
+        #region Métodos
+
+        public double calculaIMC(double pPeso, double pAltura)
+        {
+            return pPeso / (Math.Pow(pAltura,2));
+        }
+
+        public void escreveResultado(double pIMC, Pessoa pPessoa)
+        {
+            if (pIMC < 18.5)
+            {
+                Console.WriteLine("- Informações Pessoais -");
+                Console.WriteLine("¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");
+                Console.WriteLine("Nome: "+pPessoa.nome); 
+                Console.WriteLine("Sexo: "+pPessoa.sexo);
+                Console.WriteLine("Peso: "+pPessoa.peso);
+                Console.WriteLine("Altura: "+pPessoa.altura);
+                Console.WriteLine(" ");
+                Console.WriteLine(" ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ");
+                Console.WriteLine("|   - Resultado IMC -    |");
+                Console.WriteLine("|                        | ");
+                Console.WriteLine("|  Abaixo do peso ideal  |");
+                Console.WriteLine("|________________________|");
+            }else if ((pIMC >= 18.5) && (pIMC < 25))
+            {
+                Console.WriteLine("- Informações Pessoais -");
+                Console.WriteLine("¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");
+                Console.WriteLine("Nome: " + pPessoa.nome);
+                Console.WriteLine("Sexo: " + pPessoa.sexo);
+                Console.WriteLine("Peso: " + pPessoa.peso);
+                Console.WriteLine("Altura: " + pPessoa.altura);
+                Console.WriteLine(" ");
+                Console.WriteLine(" ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ");
+                Console.WriteLine("|   - Resultado IMC -    |");
+                Console.WriteLine("|                        | ");
+                Console.WriteLine("|     No peso ideal      |");
+                Console.WriteLine("|________________________|");
+            }else if ((pIMC >= 25) && (pIMC <= 30))
+            {
+                Console.WriteLine("- Informações Pessoais -");
+                Console.WriteLine("¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");
+                Console.WriteLine("Nome: " + pPessoa.nome);
+                Console.WriteLine("Sexo: " + pPessoa.sexo);
+                Console.WriteLine("Peso: " + pPessoa.peso);
+                Console.WriteLine("Altura: " + pPessoa.altura);
+                Console.WriteLine(" ");
+                Console.WriteLine(" ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ");
+                Console.WriteLine("|   - Resultado IMC -    |");
+                Console.WriteLine("|                        | ");
+                Console.WriteLine("|  Acima do peso ideal   |");
+                Console.WriteLine("|________________________|");
+            }else if (pIMC > 30)
+            {
+                Console.WriteLine("- Informações Pessoais -");
+                Console.WriteLine("¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");
+                Console.WriteLine("Nome: " + pPessoa.nome);
+                Console.WriteLine("Sexo: " + pPessoa.sexo);
+                Console.WriteLine("Peso: " + pPessoa.peso);
+                Console.WriteLine("Altura: " + pPessoa.altura);
+                Console.WriteLine(" ");
+                Console.WriteLine(" ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ");
+                Console.WriteLine("|   - Resultado IMC -    |");
+                Console.WriteLine("|                        | ");
+                Console.WriteLine("|         Obeso          |");
+                Console.WriteLine("|________________________|");
+            }
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ calculo_imc/calculo_imc/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculo_imc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            char r = 'N';
+
+            do
+            {
+                Console.WriteLine("---- CÁLCULO DE IMC ----\n");
+                Console.Write("Informe seu nome: ");
+                p.nome = Console.ReadLine();
+                Console.Write("Informe seu sexo: ");
+                p.sexo = Console.ReadLine();
+                Console.Write("Informe seu peso: ");
+                p.peso = double.Parse(Console.ReadLine());
+                Console.Write("Informe sua altura: ");
+                p.altura = double.Parse(Console.ReadLine());
+                Console.Clear();
+
+                p.escreveResultado(p.calculaIMC(p.peso, p.altura), p);
+
+                Console.Write("\nDeseja repetir o teste? (S/N): ");
+                r = char.ToUpper(char.Parse(Console.ReadLine()));
+
+                if (r.Equals('S')) {
+                    Console.Clear();
+                }
+
+            } while (r.Equals('S'));
+
+            Console.ReadLine();
+        }
+    }
+}
